fcb9b0699beaf65167f36195c2ee0bfeacc5974a
Serdıl Çağın Çakmak <38701116+lghtr35@users.noreply.github.com>
2022-11-22 11:53:05 +0300

    Update event_record.go

--- src/models/entity/event_record.go
+++ src/models/entity/event_record.go
@@ -5,11 +5,11 @@ import (
 )
 
 type EventRecord struct {
-	Id            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	EventStatus   *bool              `bson:"event_status,omitempty" json:"event_status"`
-	EventId       *int64             `bson:"event_id,omitempty" json:"event_id"`
-	Action        string             `bson:"action,omitempty" json:"action"`
-	Description   string             `bson:"description,omitempty" json:"description"`
-	VariantFields primitive.M        `bson:"variant_fields,omitempty" json:"variant_fields"`
-	CreatedAt     *int64             `bson:"created_at" json:"created_at"`
+	Id            primitive.ObjectID     `bson:"_id,omitempty" json:"id"`
+	EventStatus   *bool                  `bson:"event_status,omitempty" json:"event_status"`
+	EventId       *int64                 `bson:"event_id,omitempty" json:"event_id"`
+	Action        string                 `bson:"action,omitempty" json:"action"`
+	Description   string                 `bson:"description,omitempty" json:"description"`
+	VariantFields map[string]interface{} `bson:"variant_fields,omitempty" json:"variant_fields"`
+	CreatedAt     *int64                 `bson:"created_at" json:"created_at"`
 }
